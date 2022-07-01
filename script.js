6f82e25aff9d5d95473e0cf47505128be4ef5afa
Giedre <giedre.klima@gmail.com>
2022-07-01 15:27:30 +0300

    add style

--- script.js
+++ script.js
@@ -7,11 +7,30 @@ nameFormEl.addEventListener("submit", (event) => {
   let searchName = event.target.elements.name.value;
   console.log(searchName);
 
-  fetch(`https://agify.io?name=${searchName}`)
+  fetch(`https://api.agify.io?name=${searchName}`)
     .then((res) => res.json())
     .then((data) => {
+      console.log("age", data);
       let age = data.age;
-      let ageText = `Age of ${searchName} is ${age}.`;
-      outputEl.textContent = ageText;
+      let ageText = `Age of ${searchName} is ${age}. `;
+      outputEl.textContent += ageText;
+    });
+
+  fetch(`https://api.genderize.io?name=${searchName}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("gender", data);
+      let gender = data.gender;
+      let probability = data.probability;
+      let text = `${searchName} is ${gender} (probability ${
+        probability * 100
+      }%). `;
+      outputEl.textContent += text;
+    });
+
+  fetch(`https://api.nationalize.io?name=${searchName}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("nationalize", data);
     });
 });
